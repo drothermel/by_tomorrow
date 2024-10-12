4feb44fa7c187d6ef645c3bc4f48f254a282d667
Danielle Rothermel <danielle.rothermel@gmail.com>
2024-10-11 21:56:25 -0400

    add write_author_page with max papers and years filter

--- src/bytom/author_profiles.py
+++ src/bytom/author_profiles.py
@@ -1,9 +1,25 @@
+from enum import Enum
+from datetime import datetime
 import logging
 import re
 from pathlib import Path
 import io
 
+import dr_util.file_utils as fu
+
 import bytom.arxiv_utils as xu
+
+
+class SUMMARY_SOURCE(Enum):
+    ABSTRACT = 1
+    # NO_APPENDIX = 2
+    # FULL_PDF = 3
+
+
+class SUMMARY_FORMAT(Enum):
+    MARKDOWN = 1
+    # JSON = 2
+
 
 # --- Info Gathering Functions --- #
 
@@ -34,17 +50,62 @@ def list_authors_with_summaries(cfg, version=None):
     return list(profs)
 
 
-# --- Profile Dumping Functions --- #
+# --- Profile Writing Functions --- #
 
 
 def format_response_abstract_to_markdown(response):
     buff = io.StringIO()
 
-    buff.write(f"# **Title:** {response['title']}\n\n")
+    buff.write(f"### **Title:** {response['title']}\n\n")
     buff.write(f"**Publish Date:** {response['published']}\n\n")
     buff.write(f"**First Author:** {response['authors'][0]}\n\n")
     buff.write(f"**Last Author:** {response['authors'][-1]}\n\n")
     if len(response["authors"]) > 2:
         buff.write(f"**Middle Authors:** {', '.join(response['authors'][1:-1])}\n\n")
-    buff.write(f"**Abstract:** {response['abstract']}\n")
+    buff.write(f"**Abstract:** {response['abstract']}\n\n")
+    buff.write(f'{"-"*15}\n\n\n')
     return buff.getvalue()
+
+
+def make_author_page(
+    cfg, author, responses=None, author_info=None, max_papers=100, max_years=20
+):
+    if author_info is None:
+        author_infos = fu.load_file(cfg.author_info_file)
+        author_info = author_infos[author]
+
+    if responses is None:
+        responses = get_author_papers(author, kwargs={"max_results": max_papers})
+    else:
+        responses = responses[:max_papers]
+
+    buff = io.StringIO()
+    buff.write(f"# Research Summary for **{author}**\n\n")
+    buff.write(f"## {author} Bio\n\n")
+    buff.write(f"{author_info}\n\n")
+
+    buff.write("## Recent Papers\n\n")
+    for resp in responses:
+        published = datetime.strptime(resp["published"], "%Y-%m-%d")
+        if (datetime.now() - published).days / 365.25 < max_years:
+            buff.write(format_response_abstract_to_markdown(resp))
+    return buff.getvalue()
+
+
+def write_author_page(
+    cfg, author, version, responses=None, author_info=None, max_papers=100, max_years=20
+):
+    author_page = make_author_page(
+        cfg,
+        author,
+        responses=responses,
+        author_info=author_info,
+        max_papers=max_papers,
+        max_years=max_years,
+    )
+    author_str = author.lower().replace(" ", "_")
+    fu.dump_file(
+        author_page,
+        f"{cfg.author_summaries_dir}{author_str}.markdown."
+        f"v{version}.maxp{max_papers}.maxy{max_years}.txt",
+    )
